38d6f971fc4819bff559dc71b843c33a9c3e9ddd
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2020-08-28 15:19:59 -0400

    Hello World Java program in bluej

---
+++ gp1/HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author Karmanyaah Malhotra
+ * @version 0.0.1
+ */
+public class HelloWorld
+{
+    public static void main(String args[ ]) {
+        
+        //your code here
+        System.out.println("Hello, World!");
+    }
+}
