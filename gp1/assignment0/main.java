fc5987ff90eaf98f51fab244d58d10a936da666d
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2020-08-31 13:03:30 -0400

    Add Programming Assignment: 0

---
+++ gp1/assignment0/main.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class main here.
+ *
+ * @author (Karmanyaah Malhotra)
+ * @version (8/31/2020)
+ * @assignment (Programming Assignment 0)
+ */
+public class main
+{
+    public static void main(String args[]){
+        String first = "Karmanyaah";
+        String last = "Malhotra";
+
+        for (char letter : first.toCharArray()){
+            System.out.println(letter);
+        }
+        System.out.println(last);
+    }
+}
