3f281a8aa6fa5a041d6baf38de8f8ec59ec827e6
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2020-09-09 00:16:37 -0400

    chore: repo maintainance

---
+++ gp1/tmp/main.java
@@ -0,0 +1,14 @@
+/**
+ * Write a description of class HelloWorld here.
+ *
+ * @author (Karmanyaah Malhotra)
+ * @version (a version number or a date)
+ * @assignment (stub)
+ */
+public class main
+{
+    public static void main(String args[ ]) {
+        int firstInt=1,secondInt = 2; //camel case
+
+        System.out.println("lol " + (firstInt +secondInt));   }
+}
