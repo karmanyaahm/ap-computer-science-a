4a99ce4af295c2acdd0d83c5072bd852f057468e
Karmanyaah Malhotra <karmanyaahm@gmail.com>
2020-11-04 14:53:56 -0500

    TwoRobots

--- gp2/firstKarel/TwoRobots.java
+++ gp2/firstKarel/TwoRobots.java
@@ -8,13 +8,13 @@
 
 import kareltherobot.*;
 
-public class TwoRobots implements Directions
-{
+public class TwoRobots implements Directions {
 
     public static void main(String args[]) {
 
         //World is a class in the Karel jar file
-        World.readWorld("../worlds/fig2-5.kwld");  //gives us a blank grid
+        World.readWorld("fig2-5.kwld");  //gives us a blank grid
+        World.setDelay(25);
         World.setVisible(true);
 
         //create a robot object
@@ -25,6 +25,37 @@ public class TwoRobots implements Directions
 
         //get the gift
         carolyn.pickBeeper();
+
+        carolyn.move();
+        carolyn.turnLeft();
+        carolyn.turnLeft();
+        carolyn.turnLeft();
+        carolyn.move();
+
+        //leave gift
+        carolyn.putBeeper();
+
+        //go back to start
+        carolyn.turnLeft();
+        carolyn.turnLeft();
+        carolyn.move();
+        carolyn.turnLeft();
+        carolyn.move();
+
+        //karel goes to 2,2
+        karel.move();
+        karel.turnLeft();
+        karel.move();
+
+        //get gift
+        karel.pickBeeper();
+
+        //spin around with joy
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+        karel.turnLeft();
+
 
     }
 }
